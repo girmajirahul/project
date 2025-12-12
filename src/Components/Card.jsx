8c1670c000a18bcc171c680ae0e643bbf915d27e
RSG-developer <girmajirahul2@gmail.com>
2025-12-12 19:19:52 +0530

    candidate

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -9,7 +9,7 @@ export default function Card({ data, type }) {
   console.log(data);
   const navigate=useNavigate();
   return (
-    <div className="ml-20 rounded-lg shadow-2xl overflow-hidden p-7">
+    <div className="ml-10 rounded-lg shadow-2xl overflow-hidden p-7">
      
      {type==="candidate" &&(
         <>
